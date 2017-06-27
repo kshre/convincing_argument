aeb842544b2dee6f2c09d04efe8217deb64151a8
Saurabh <saurkulsh@gmail.com>
2017-06-27 16:09:59 -0400

    added the file that will contain most of the code

--- pretrained_word_embeddings.py
+++ pretrained_word_embeddings.py
@@ -1,8 +1,4 @@
-# convincing_argument
-
-pretrained_word_embeddings.py:
-
-
+"""
 This file will use pretrained word embeddings to prepare representations
 of the argument and the wikipedia articles, then do a sum over the dot
 product of the representation of the argument with each wikipedia article
@@ -10,3 +6,4 @@ and finally use the sum as the representation of argument combined with
 knowledge from wikipedia. This final combined representations of two
 articles will be used to do classification between the two for which one
 is more convincing using logistic regression.
+"""
